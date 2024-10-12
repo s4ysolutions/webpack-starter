df3734919e625d5eabff46f4993b57ed561318cf
dsa <sergey@s4y.solutions>
2024-10-12 19:25:44 +0200

    ESLint fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import HelloWorld from './react/components/hello-world';
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import HelloWorld from './react/components/hello-world';
 
 const container = document.getElementById('reactMount');
 const root = createRoot(container);
--- tests/mocha/helloworld.spec.ts
+++ tests/mocha/helloworld.spec.ts
@@ -1,20 +1,21 @@
-import {expect, use as chaiUse} from 'chai';
+import {use as chaiUse, expect} from 'chai';
+import {HelloWorldClass} from '../../src/hello-world';
 import chaiString from 'chai-string';
 import sinon from 'sinon';
-import {HelloWorldClass} from '../../src/hello-world';
-chaiUse(chaiString)
+
+chaiUse(chaiString);
 
 describe('hello world mocha test', () => {
-  const suffix = 'from HelloWorldClass'
+  const suffix = 'from HelloWorldClass';
   it('chai is working', () => {
-    const helloWorld = new HelloWorldClass(suffix)
-    expect(helloWorld.getHello()).to.endsWith(suffix)
+    const helloWorld = new HelloWorldClass(suffix);
+    expect(helloWorld.getHello()).to.endsWith(suffix);
   });
   it('sinon is working', () => {
-    const helloWorld = new HelloWorldClass(suffix)
-    const name = 'aaa'
-    const stub = sinon.stub(HelloWorldClass, 'getName')
-    stub.returns(name)
-    expect(helloWorld.getHello()).to.equals(`Hello, ${name} - ${suffix}`)
+    const helloWorld = new HelloWorldClass(suffix);
+    const name = 'aaa';
+    const stub = sinon.stub(HelloWorldClass, 'getName');
+    stub.returns(name);
+    expect(helloWorld.getHello()).to.equals(`Hello, ${name} - ${suffix}`);
   });
 });
